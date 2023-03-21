4a2a8927313884b412801b46e24891745571e5ba
Erhan <erhan.citil@maykinmedia.nl>
2023-03-21 19:32:08 +0100

    🚧 Index view url pattern changed

--- src/nederlandse_spoorwegen/nsapi/urls.py
+++ src/nederlandse_spoorwegen/nsapi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import Index, StationView, StationDetailView
 
 urlpatterns = [
-    path('index/', Index.as_view(), name='index'),
+    path('', Index.as_view(), name='index'),
     path('stations/', StationView.as_view(), name='stations'),
     path('station/<str:uiccode>', StationDetailView.as_view(), name='station')
 ]
